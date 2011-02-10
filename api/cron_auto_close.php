c2c7a94c98463bccf424c8f34e0b6154eda23f29
Robert Middleswarth <robert@middleswarth.net>
2011-02-10 00:23:17 -0500

    Add in the meat of the code.  Still have the update commented out for safety until testing.

--- api/cron_auto_close.php
+++ api/cron_auto_close.php
@@ -11,7 +11,46 @@ class Cerb5BlogAutoCloseCron extends CerberusCronPageExtension {
 
 		@$ac_only_unassigned = $this->getParam('only_unassigned', 0);
 		@$ac_close_days = $this->getParam('close_days', 7);
- 
+		
+		$sql = "SELECT t.id, ";
+		$sql .= "t.subject, t.created_date, t.updated_date, t.is_closed, ";
+		$sql .= "t.is_waiting, t.team_id, t.category_id ";
+		$sql .= "FROM ticket t ";
+		$sql .= sprintf("WHERE t.updated_date > %d  ", strtotime("+".$ac_close_days." days"));
+		$sql .= "AND t.is_waiting = 1 ";
+		$sql .= "GROUP BY t.id ";
+		$sql .= "ORDER BY t.id ";
+		$tpl->assign('sql', $sql);
+		
+		$rs = $db->Execute($sql) or $db->ErrorMsg(); 
+		while($row = mysql_fetch_assoc($rs)) {
+			// Loop though the records.
+			$id = intval($row['id']);
+			
+			$context_workers = CerberusContexts::getWorkers(CerberusContexts::CONTEXT_TICKET, $id);
+			if(($ac_only_unassigned == 1) && (count($context_workers)>0)) {
+				// Do something.
+			} else {
+				if (DevblocksPlatform::isPluginEnabled('cerberusweb.auditlog')) {
+					// Code that requires time tracker to be enabled.
+					$fields = array(
+						DAO_TicketAuditLog::TICKET_ID => $id,
+						DAO_TicketAuditLog::WORKER_ID => 0,
+						DAO_TicketAuditLog::CHANGE_DATE => time(),
+						DAO_TicketAuditLog::CHANGE_FIELD => "cerb5blog.auto_close.auto_closed",
+						DAO_TicketAuditLog::CHANGE_VALUE => "Auto Closed",
+					);
+					$log_id = DAO_TicketAuditLog::create($fields);
+					unset($fields);
+				}
+		
+				$fields[DAO_Ticket::IS_WAITING] = 0;
+				$fields[DAO_Ticket::IS_CLOSED] = 1;
+				$fields[DAO_Ticket::IS_DELETED] = 0;
+				//DAO_Ticket::update($id, $fields);
+				unset($fields);
+			}
+		}
 		$logger->info("[Cerb5Blog.com] Finished processing Auto Close Cron Job.");
   }
  
