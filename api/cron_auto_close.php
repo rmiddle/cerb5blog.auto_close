022fd40f87bf9286e384486c1e8b6dfed6134864
Robert Middleswarth <robert@middleswarth.net>
2011-02-10 01:12:25 -0500

    That should be the last one.

--- api/cron_auto_close.php
+++ api/cron_auto_close.php
@@ -4,6 +4,7 @@ class Cerb5BlogAutoCloseCron extends CerberusCronPageExtension {
   const EXTENSION_ID = 'cerb5blog.auto_close.cron';
 
 	function run() {
+		$db = DevblocksPlatform::getDatabaseService();
 		$logger = DevblocksPlatform::getConsoleLog();
 		$logger->info("[Cerb5Blog.com] Running Auto Close Cron Task.");
 
@@ -12,14 +13,13 @@ class Cerb5BlogAutoCloseCron extends CerberusCronPageExtension {
 		@$ac_only_unassigned = $this->getParam('only_unassigned', 0);
 		@$ac_close_days = $this->getParam('close_days', 7);
 		
-		$sql = "SELECT t.id, ";
-		$sql .= "t.subject, t.created_date, t.updated_date, t.is_closed, ";
-		$sql .= "t.is_waiting, t.team_id, t.category_id ";
+		$sql = "SELECT t.id ";
 		$sql .= "FROM ticket t ";
-		$sql .= sprintf("WHERE t.updated_date > %d  ", strtotime("+".$ac_close_days." days"));
+		$sql .= sprintf("WHERE t.updated_date < %d  ", strtotime("-".$ac_close_days." days"));
 		$sql .= "AND t.is_waiting = 1 ";
 		$sql .= "GROUP BY t.id ";
 		$sql .= "ORDER BY t.id ";
+		$logger->info("[Cerb5Blog.com] SQL = " . $sql);
 		
 		$rs = $db->Execute($sql);
 		while($row = mysql_fetch_assoc($rs)) {
@@ -28,10 +28,10 @@ class Cerb5BlogAutoCloseCron extends CerberusCronPageExtension {
 			
 			$context_workers = CerberusContexts::getWorkers(CerberusContexts::CONTEXT_TICKET, $id);
 			if(($ac_only_unassigned == 1) && (count($context_workers)>0)) {
-				// Do something.
+				$logger->info("[Cerb5Blog.com] Worker assigned but we are only closing tickets without a worker.");
 			} else {
 				$logger->info("[Cerb5Blog.com] Closing Ticket # " . $id);
-				if (DevblocksPlatform::isPluginEnabled('cerberusweb.auditlog')) {
+				if (class_exists('DAO_TicketAuditLog',true)):
 					// Code that requires time tracker to be enabled.
 					$fields = array(
 						DAO_TicketAuditLog::TICKET_ID => $id,
@@ -42,12 +42,12 @@ class Cerb5BlogAutoCloseCron extends CerberusCronPageExtension {
 					);
 					$log_id = DAO_TicketAuditLog::create($fields);
 					unset($fields);
-				}
+				endif;
 		
 				$fields[DAO_Ticket::IS_WAITING] = 0;
 				$fields[DAO_Ticket::IS_CLOSED] = 1;
 				$fields[DAO_Ticket::IS_DELETED] = 0;
-				//DAO_Ticket::update($id, $fields);
+				DAO_Ticket::update($id, $fields);
 				unset($fields);
 			}
 		}
